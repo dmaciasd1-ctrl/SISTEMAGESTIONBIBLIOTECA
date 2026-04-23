c16597c37cc359fadf7d1f832e7d6c13fd06b6c9
dmaciasd1-ctrl <dmaciasd1@unicartagena.edu.co>
2026-04-23 19:35:57 -0400

    comit2
    clase libro y prestamo

--- src/main/java/modelo/Libro.java
+++ src/main/java/modelo/Libro.java
@@ -10,4 +10,32 @@ package modelo;
  */
 public class Libro {
     
+    private final String isbn;
+    private final String titulo;
+    private Autor autor;        // Asociación
+    private Categoria categoria; // Dependencia
+
+    public Libro(String isbn, String titulo, Autor autor, Categoria categoria) {
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.categoria = categoria;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+    
 }
--- src/main/java/modelo/Prestamo.java
+++ src/main/java/modelo/Prestamo.java
@@ -3,11 +3,41 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package modelo;
-
+import java.time.LocalDate;
 /**
  *
  * @author ESTUDIANTE
  */
 public class Prestamo {
+    private static int contadorId = 0;
+    private final int id;
+    private Libro libro;
+    private String usuario;
+    private LocalDate fechaSalida;
+    private LocalDate fechaDevolucion;
+
+    public Prestamo(Libro libro, String usuario) {
+        this.id = generarNuevoId();
+        this.libro = libro;
+        this.usuario = usuario;
+        this.fechaSalida = LocalDate.now();
+    }
+    
+    private static int generarNuevoId() {
+        return ++contadorId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Libro getLibro() {
+        return libro;
+    }
+
+    public void setFechaDevolucion(LocalDate fechaDevolucion) {
+        this.fechaDevolucion = fechaDevolucion;
+    }
+    
     
 }
